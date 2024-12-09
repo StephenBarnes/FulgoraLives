2a961cbeb18666ab0791253cdc41f3af962c60cf
Stephen Barnes <stephen6174@gmail.com>
2024-12-09 14:33:20 +0200

    Added solid fuel to scrap recycling products for any-planet-start compat

--- code/data/any-planet-start-du.lua
+++ code/data/any-planet-start-du.lua
@@ -6,9 +6,10 @@ if mods["any-planet-start"] ~= nil and settings.startup["aps-planet"].value == "
 	-- We want to avoid softlocking in the case of using Any Planet Start mod and starting on Fulgora.
 	-- One issue: lightning rod needs stone bricks, which needs solid fuel. Or you could recycle concrete, but that needs power, so needs lightning rods.
 	-- Well, you can get some stone bricks from mining lightning rods - each mined rod gives 2, new rods cost 4. But that's still tedious.
-	-- Just change the recipe to use concrete instead of stone bricks.
-	Util.substituteIngredient("lightning-rod", "stone-brick", "concrete")
-	-- Also need stone bricks later, eg for bricks for military science. But at that point you have have oil processing.
+	-- You also need fuel for burner miners and furnaces. So, changing scrap recycling recipe to give solid fuel too.
+	table.insert(data.raw.recipe["scrap-recycling"].results,
+		{ type = "item",  name = "solid-fuel",  amount = 1, probability = 0.04, show_details_in_recipe_tooltip = false })
+	--Util.substituteIngredient("lightning-rod", "stone-brick", "concrete")
 
 	-- If Any Planet Start mod is installed, sludge filtration isn't usable until you have "oil-gathering" technology for offshore pump.
 	-- And you don't have water, so for blue science you need some other recipe for sulfur. And for military, some other recipe for coal.
