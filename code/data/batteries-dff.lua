2ca1f94648950d62b20268a2110b5a2221a22ca9
Stephen Barnes <stephen6174@gmail.com>
2024-12-14 09:56:06 +0200

    Make charged battery recycling give back un-charged batteries

--- code/data/batteries-dff.lua
+++ code/data/batteries-dff.lua
@@ -10,3 +10,11 @@ for _, recipe in pairs(data.raw.recipe) do
 	end
 end
 Util.hideRecipe("accumulator-recycling")
+
+-- Change charged battery recycling recipes to just discharge the batteries.
+if data.raw.recipe["charged-battery-recycling"] then
+	data.raw.recipe["charged-battery-recycling"].results = {{type = "item", name = "battery", amount = 1}}
+end
+if data.raw.recipe["charged-holmium-battery-recycling"] then
+	data.raw.recipe["charged-holmium-battery-recycling"].results = {{type = "item", name = "holmium-battery", amount = 1}}
+end
